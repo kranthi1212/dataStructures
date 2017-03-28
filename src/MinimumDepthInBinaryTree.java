3e65112920ddda011f0dbd31942dd1cad687e48e
KSabbani <Kranthi.Sabbani@state.nm.us>
2017-03-27 18:03:11 -0600

    new program

--- src/MinimumDepthInBinaryTree.java
+++ src/MinimumDepthInBinaryTree.java
@@ -23,8 +23,8 @@ public class MinimumDepthInBinaryTree {
     public static void main(String[] args) {
 	Node root = new Node(1);
 	root.left = new Node(2);
-	root.right = new Node(3);
-	root.left.left = new Node(4);
+	// root.right = new Node(3);
+	// root.left.left = new Node(4);
 	// root.left.right = new Node(5);
 
 	MinimumDepthInBinaryTree minDepth = new MinimumDepthInBinaryTree();
@@ -33,20 +33,29 @@ public class MinimumDepthInBinaryTree {
     }
 
     private void findMinDepth(Node root) {
-	int minLeftDepth = this.findMinDepth1(root, 0);
-	// int minRightDepth = this.findMinDepth1(root.right, 0);
+	int minLeftDepth = this.findMinDepth1(root);
 	System.out.println("MinDepth = " + minLeftDepth);
     }
 
-    private int findMinDepth1(Node root, int k) {
+    private int findMinDepth1(Node root) {
+	// Corner case. Should never be hit unless the code is
+	// called on root = NULL
 	if (root == null)
-	    return k;
-	if (root.right == null)
-	    return findMinDepth1(root.left, k + 1);
-	if (root.left == null)
-	    return findMinDepth1(root.right, k + 1);
+	    return 0;
 
-	return Math.min(findMinDepth1(root.left, k + 1), findMinDepth1(root.right, k + 1));
+	// Base case : Leaf Node. This accounts for height = 1.
+	if (root.left == null && root.right == null)
+	    return 1;
+
+	// If left subtree is NULL, recur for right subtree
+	if (root.left == null)
+	    return findMinDepth1(root.right) + 1;
+
+	// If right subtree is NULL, recur for right subtree
+	if (root.right == null)
+	    return findMinDepth1(root.left) + 1;
+
+	return Math.min(findMinDepth1(root.left), findMinDepth1(root.right)) + 1;
 
     }
 }
